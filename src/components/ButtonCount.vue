38f0e0e0dc2d61ba25507878ca6fa98fab5b3bf5
phullt <phuqn10xxx@gmail.com>
2024-02-06 16:16:59 +0700

    first commit

---
+++ src/components/ButtonCount.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps({
+  plus: {
+    type: Number,
+    required: true
+  },
+})
+</script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods :{
+    HandleCount()  {
+      this.count = this.count + this.plus
+    }
+  },
+  created() {
+    console.log(this.plus) // => 2
+  }
+}
+
+
+
+</script>
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="HandleCount"  >Click</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ButtonCount from '@/components/ButtonCount.vue'
+</script>
+<script>
+export default {
+  data() {
+    return {
+      input: 0
+    }
+  }
+
+}
+</script>
+<template>
+  <main>
+    <TheWelcome />
+    <input type="number" @change="e => input = parseInt(e.target.value) " />
+    <ButtonCount :plus="input" />
+  </main>
+</template>
